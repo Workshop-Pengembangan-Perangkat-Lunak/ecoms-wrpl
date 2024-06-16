6d2d026d23fc04f1f9e1efa9f1f904c9e8e9a01b
Irfan F Rafif <irfanfrafif@gmail.com>
2024-06-16 11:49:43 +0700

    delivery

--- delivery/models.py
+++ delivery/models.py
@@ -4,7 +4,6 @@ from django.contrib.auth.models import User
 class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return self.name
