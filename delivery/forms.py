a54f180dd268113f5646e2735be513a9a58b7a4a
Irfan F Rafif <irfanfrafif@gmail.com>
2024-06-16 11:49:43 +0700

    delivery

--- delivery/forms.py
+++ delivery/forms.py
@@ -4,7 +4,6 @@ from .models import Delivery, Product
 class DeliveryForm(forms.ModelForm):
     product_name = forms.CharField(max_length=255)
     product_description = forms.CharField(widget=forms.Textarea)
-    product_price = forms.DecimalField(max_digits=10, decimal_places=2)
 
     class Meta:
         model = Delivery
@@ -14,7 +13,6 @@ class DeliveryForm(forms.ModelForm):
         product = Product(
             name=self.cleaned_data['product_name'],
             description=self.cleaned_data['product_description'],
-            price=self.cleaned_data['product_price'],
         )
         if commit:
             product.save()
